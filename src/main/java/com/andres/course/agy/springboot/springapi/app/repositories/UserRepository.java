21fac069e17263159a721063175ae0b26c78ce41
Andres Guzman <andresguzf@gmail.com>
2026-07-13 13:15:36 -0400

    quitamos el decorador repository en el jparepository

--- src/main/java/com/andres/course/agy/springboot/springapi/app/repositories/UserRepository.java
+++ src/main/java/com/andres/course/agy/springboot/springapi/app/repositories/UserRepository.java
@@ -2,13 +2,10 @@ package com.andres.course.agy.springboot.springapi.app.repositories;
 
 import com.andres.course.agy.springboot.springapi.app.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    
+
     /**
      * Find a user by their email address.
      * 
